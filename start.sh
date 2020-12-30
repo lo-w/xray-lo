ed6de51b42b745216526f5362e348bed86ec5cd7
lo <40700499+lo-w@users.noreply.github.com>
2020-12-30 12:40:37 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 # configs
-mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
-wget $CADDYIndex -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" >/xray.json
+# caddy
+mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" > /usr/share/caddy/robots.txt
+wget -qO  /usr/share/caddy/index.html $CADDYIndex
+wget -qO- $CONFIGCADDY | sed -e "s/\$AUUID/$AUUID/g" > /etc/caddy/caddy.json
 
-# storefiles
-mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
-wget -P /usr/share/caddy/$AUUID -i /usr/share/caddy/$AUUID/StoreFiles
-
-for file in $(ls /usr/share/caddy/$AUUID); do
-    [[ "$file" != "StoreFiles" ]] && echo \<a href=\""$file"\" download\>$file\<\/a\>\<br\> >>/usr/share/caddy/$AUUID/ClickToDownloadStoreFiles.html
-done
+# xray
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" > /etc/xray.json
 
 # start
-/xray -config /xray.json &
+/xray -config /etc/xray.json &
 caddy run --config /etc/caddy/caddy.json
