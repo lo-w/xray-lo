51ca118474eef9accd6a29f279036d78db03eab9
ewuxren <renjun.wu@ericsson.com>
2022-05-16 17:16:32 +0800

    remove caddy to test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 FROM alpine:edge
 RUN apk update && \
-    apk add --no-cache ca-certificates caddy wget && \
+    apk add --no-cache ca-certificates wget && \
     wget -qO- https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip | busybox unzip - && \
     chmod +x /xray && \
+    mv /xray /x && \
     rm -rf /var/cache/apk/*
 
 ADD start.sh /start.sh
--- start.sh
+++ start.sh
@@ -2,13 +2,13 @@
 
 # configs
 # caddy
-mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" > /usr/share/caddy/robots.txt
-wget -qO  /usr/share/caddy/index.html $CADDYIndex
-wget -qO- $CONFIGCADDY | sed -e "s/\$AUUID/$AUUID/g" > /etc/caddy/caddy.json
+# mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" > /usr/share/caddy/robots.txt
+# wget -qO  /usr/share/caddy/index.html $CADDYIndex
+# wget -qO- $CONFIGCADDY | sed -e "s/\$AUUID/$AUUID/g" > /etc/caddy/caddy.json
 
 # xray
 wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$PORT/$PORT/g" > /etc/xray.json
 
 # start
-caddy start --config /etc/caddy/caddy.json
-/xray -config /etc/xray.json
+# caddy start --config /etc/caddy/caddy.json
+/x -config /etc/xray.json
